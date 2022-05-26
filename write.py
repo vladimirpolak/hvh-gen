e3158ed4055fd7cfa8e0e9d257973bdde9257507
Vladimir <htcvlado@gmail.com>
2022-05-26 20:51:58 +0200

    Added link for pyexcel

--- write.py
+++ write.py
@@ -2,8 +2,10 @@ from pyexcel_ods3 import save_data
 from setgen import generate_set
 from collections import defaultdict
 
+# https://pypi.org/project/pyexcel-ods3/
+
 data = defaultdict(list)
-for _ in range(100):
+for _ in range(1000):
     data["Sheet 1"].append(generate_set())
 
-save_data("your_file.ods", data)
+save_data("example.ods", data)
